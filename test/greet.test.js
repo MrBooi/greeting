9930eb1fe06c012b90b5797fe167e6ce4d037ea3
Mr Booi <ayabongabooi2@gmail.com>
2018-04-17 16:46:52 +0200

    spelling changes

--- test/greet.test.js
+++ test/greet.test.js
@@ -32,7 +32,7 @@ describe('The greeting function', function() {
     assert.equal(addToStorage.message(), "Molo, Xola");
 
   });
-  it('should display "Hello, Andre" has a greet message if user entered Xola has a name and click on the radio button English ', function() {
+  it('should display "Hello, Andre" has a greet message if user entered Xola has a name and click on the radio button English', function() {
     var addToStorage = NamesGreetedLocalStorage();
     addToStorage.names("Andre");
     addToStorage.langauge("English");
